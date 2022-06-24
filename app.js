35b4c2d7e2cfcb44e6477f8e86128c1676c90832
guillermo vicente prieto <gvicenteprieto@gmail.com>
2022-06-24 15:26:24 -0300

    add public folder

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
 app.get ('/mensaje', (req, res) => {
     res.send('Hello Heroku!');
 });
 
-app.get ('/', (req, res) => {
-    res.send('Hello Heroku HOME!');
-});
+// app.get ('/', (req, res) => {
+//     res.send('HOME!');
+// });
 
 app.get ('/hi', (req, res) => {
     res.send('hi ho let´s go! 🚀');
---
+++ public/js/main.js
@@ -0,0 +1,1 @@
+console.log('app node desde heroku');
