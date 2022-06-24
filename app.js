81da907e233a660165bc768232f0da7763761275
guillermo vicente prieto <gvicenteprieto@gmail.com>
2022-06-24 13:04:22 -0300

    make it better

--- app.js
+++ app.js
@@ -1,11 +1,6 @@
 const express = require('express');
 const app = express();
 
-// app.use(express.static('public'));
-
-// app.get('/', (req, res) => {
-//     res.sendFile(__dirname + '/index.html');
-// });
 app.get ('/mensaje', (req, res) => {
     res.send('Hello Heroku!');
 }
